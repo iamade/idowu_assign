e45838110a23b60e51646a68be016c6bac708d6f
Adesegun Koiki <adesegun.koiki@sterling.ng>
2022-12-11 16:29:36 +0100

    spiral done

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -4,7 +4,33 @@ def Exercise2(matrix):
     up = left = 0
     right = columns - 1
     down = rows - 1
+
     while len(result) < rows * columns:
-        Travserve from left to 
-    for i in matrix:
-        
+        # Travserve from left to right
+        for col in range(left, right+1):
+            result.append(matrix[up][col])
+ 
+        # Traverse downwards
+        for row in range(up +1, down +1):
+            result.append(matrix[row][right])
+
+        # make sure we are now on a differenet row
+        if up != down:
+            # Traverse from right to left
+            for col in range(right - 1, left -1, -1):
+                result.append(matrix[down][col])
+
+        # make sure we are now on a differenet column
+        if left != right:
+            # Traverse upwards
+            for row in range(down - 1, up, -1):
+                result.append(matrix[row][left])
+
+        left += 1
+        right -= 1
+        up += 1
+        down -= 1
+    return result        
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]] 
+print(Exercise2(matrix))
